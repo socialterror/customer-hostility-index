521dd8009d2634fc87474b449a610f3857aa17c8
socialterror <jlonghitano@gmail.com>
2026-08-18 15:25:53 +0200

    Trigger H06 final review chunks

--- h06_temp/chunk_final41_review.py
+++ h06_temp/chunk_final41_review.py
@@ -8,4 +8,4 @@ for lo,hi,name in [(204,213,'realestate_tail'),(214,225,'utilities')]:
   if lo<=o<=hi:
    lines += [f'## {o:03d} {r["ticker"]}', 'F1 '+r['F1_best'][:650], 'F5 '+r['F5_best'][:650], 'MID '+r['mid_best'][:650], '']
  (ROOT/'t0_review'/f'{name}_review.md').write_text('\n'.join(lines),encoding='utf-8')
-print('done')
+print('done',len(rows))
